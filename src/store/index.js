88551f869714c093571a6af39a5900e1cdf6b2b7
Hashani26 <hashanilakshi@gmail.com>
2025-01-11 14:54:35 +0530

    remove unwanted imports]

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
-// import { fetchUsers } from "./thunks/fetchUsers";
 
 export const store = configureStore({
   reducer: {
