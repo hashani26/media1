b41a6a7e5533d27dd4ae83b664c610f8131e1fbb
Hashani26 <hashanilakshi@gmail.com>
2025-01-11 14:51:28 +0530

    StrictMode creates two network calls for dispatch or pure functions in dev mode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.js";
 import { store } from "./store";
@@ -6,9 +5,7 @@ import { Provider } from "react-redux";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>
 );
