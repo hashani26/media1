9d04b5b8a4d2772e323e1d986f18527ddf3d7ae7
Hashani26 <hashanilakshi@gmail.com>
2025-01-11 12:23:09 +0530

    comapre fetch thunk

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -2,9 +2,20 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
-  const response = await axios.get("http://localhost:3005/user");
+  const response = await axios.get("http://localhost:3005/users");
+  console.log("🚀 ~ fetchUsers ~ response:", response)
+  // dev only to display loading state
+  await pause(1000);
   return response.data;
 });
+
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve;
+    }, duration);
+  });
+};
 
 export { fetchUsers };
 
